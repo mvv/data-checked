32901be75498ef1a2fdd2b11810cf81e323a41bf
Mikhail Vorozhtsov <mikhail.vorozhtsov@gmail.com>
2013-01-31 16:46:50 +0700

    Initial import.

---
+++ README.md
@@ -0,0 +1,11 @@
+Data-Checked
+============
+This package provides a (phantom) type-indexed newtype evidence-wrapper for
+values that are checked to satisfy the property associated with the type. 
+
+Installation
+------------
+The usual:
+
+	$ cabal install
+
---
+++ src/Data/Checked.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+-- | Type-indexed runtime-checked properties.
+module Data.Checked
+  ( Checked
+  , trustMe
+  , checked
+  , Property(..)
+  , maybeHolds
+  , check
+  ) where
+
+-- | Wrapper-evidence for property @p@.
+newtype Checked p v = Checked v
+
+-- | Use when the property can be deduced without a runtime check.
+trustMe ∷ v → Checked p v
+trustMe = Checked
+
+-- | Unwrap the checked value.
+checked ∷ Checked p v → v
+checked (Checked v) = v
+
+class Property p v where
+  -- | Test if the property holds for the given value.
+  --   The first argument is supposed to be ignored.
+  holds ∷ p → v → Bool
+
+-- | Return @Just v@ if @p@ holds and @Nothing@ overwise.
+maybeHolds ∷ Property p v ⇒ p → v → Maybe v
+maybeHolds p v | holds p v = Just v
+               | otherwise = Nothing
+
+-- | Wrap the value if the property holds.
+check ∷ ∀ p v . Property p v ⇒ v → Maybe (Checked p v)
+check v | holds (undefined ∷ p) v = Just (Checked v)
+        | otherwise               = Nothing
+
