6adc17c072c67837b05cf642f1f86fb500dbcfe5
Mikhail Vorozhtsov <mikhail.vorozhtsov@gmail.com>
2013-01-31 18:37:15 +0700

    Added inline pragmas.

--- src/Data/Checked.hs
+++ src/Data/Checked.hs
@@ -18,10 +18,12 @@ newtype Checked p v = Checked v
 -- | Use when the property can be deduced without a runtime check.
 trustMe ∷ v → Checked p v
 trustMe = Checked
+{-# INLINE trustMe #-}
 
 -- | Unwrap the checked value.
 checked ∷ Checked p v → v
 checked (Checked v) = v
+{-# INLINE checked #-}
 
 class Property p v where
   -- | Test if the property holds for the given value.
@@ -32,9 +34,11 @@ class Property p v where
 maybeHolds ∷ Property p v ⇒ p → v → Maybe v
 maybeHolds p v | holds p v = Just v
                | otherwise = Nothing
+{-# INLINABLE maybeHolds #-}
 
 -- | Wrap the value if the property holds.
 check ∷ ∀ p v . Property p v ⇒ v → Maybe (Checked p v)
 check v | holds (undefined ∷ p) v = Just (Checked v)
         | otherwise               = Nothing
+{-# INLINABLE check #-}
 
